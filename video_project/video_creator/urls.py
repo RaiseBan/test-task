5593683881a8294b8385dbfd5b4b766f14644d6a
Sergey <shornikov1407@gmail.com>
2024-09-17 13:35:04 +0300

    :bug: fix path, delete '/' at the end of the path

--- video_project/video_creator/urls.py
+++ video_project/video_creator/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create-video/', views.create_video, name='create_video'),
+    path('create-video', views.create_video, name='create_video'),
 ]
